3da7cfb4a5eb4aea85650f2a864f86991dac32c1
Matt <burtonmatthew00@gmail.com>
2019-02-15 10:39:19 +1000

    basic update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title> Brisbane Fuel Scab </title>
-	<link rel="stylesheet" href="new_style.css">
+	<link rel="stylesheet" href="css/new_style.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
